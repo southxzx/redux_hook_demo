75a3b460ad9f9f2ff18178f41138e7cba0f8112a
southxz <southxzx109@users.noreply.github.com>
2020-10-07 10:59:52 +0700

    final
    done

--- src/components/Home/HobbyList.js
+++ src/components/Home/HobbyList.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './HobbyList.css';
 
 HobbyList.propTypes = {
-    hobbyList: PropTypes.array
+    hobbyList: PropTypes.array,
+    activeID: PropTypes.number,
+    onHobbyClick: PropTypes.func
 };
 
 HobbyList.defaultProps = {
-    hobbyList : []
+    hobbyList : [],
+    activeID: null,
+    onHobbyClick: null
 }
 
 function HobbyList(props) {
-    const { hobbyList } = props;
+    const { hobbyList, activeID, onHobbyClick } = props;
+
+    const handleClick = (hobby) => {
+        if(onHobbyClick){
+            // Hàm bên hàm cha HomePage
+            onHobbyClick(hobby);
+        }
+    }
+
     return (
-        <ul>
+        <ul className="hobby-list">
             {hobbyList.map(hobby => (
-                <li key={hobby.id}>{hobby.title}</li>
+                <li key={hobby.id} className={hobby.id === activeID ? 'active' : ''} onClick={() =>handleClick(hobby)}>{hobby.title}</li>
             ))}
         </ul>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import HobbyList from '../components/Home/HobbyList';
-import casual from 'casual';
+// import casual from 'casual';
+import { addNewHobby, setActivityHobby } from '../actions/hobby';
 
+
+const randomNumber = () => {
+    return 1000 + Math.trunc(Math.random() * 9000);
+}
 
 function HomePage(props) {
 
     const hobbyList = useSelector(state => state.hobby.list);
+    const activeID = useSelector(state => state.hobby.activeID);
     const dispatch = useDispatch();
 
-    //console.log(hobbyList);
+    console.log(hobbyList);
 
     const handleAddHobby = () => {
         // Random a hobby obj: id + title
+        const newId = randomNumber();
         const newHobby = {
-            id: casual.uuid,
-            title: casual.title
+            id: newId,
+            title: `DMM ${newId}`
         }
         // Add
-        const action = addNewHobby
+        const action = addNewHobby(newHobby);
+        dispatch(action);
+    }
+
+    const handleHobbyClick = (hobby) => {
+        const action = setActivityHobby(hobby);
+        dispatch(action);
     }
 
     return (
@@ -26,7 +39,11 @@ function HomePage(props) {
             <h3>Redux Hook Homepage</h3>
 
             <button onClick={handleAddHobby}>Random Hobby</button>
-            <HobbyList hobbyList={hobbyList}></HobbyList>
+            <HobbyList 
+                hobbyList={hobbyList}
+                activeID={activeID}
+                onHobbyClick={handleHobbyClick}
+                />
         </div>
     )
 }
